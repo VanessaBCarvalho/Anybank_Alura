05f580c521b2e018c8f861ed013348b248a1654e
Vanessa <vanessa_bcarvalho@hotmail.com>
2026-04-08 16:55:21 -0300

    transacao

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
 import { LateralComponent } from './components/lateral/lateral.component';
+import { FormNovaTransacaoComponent } from './form-nova-transacao/form-nova-transacao.component';
+import { Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent, LateralComponent],
+  imports: [BannerComponent, LateralComponent, FormNovaTransacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'anybank';
+  processarTransacao(transacao: Transacao) {
+    console.log(transacao)
+  }
 }
